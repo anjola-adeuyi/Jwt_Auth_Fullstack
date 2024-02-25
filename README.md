bbe66d28e2b3e98cb93a4cd1358cdafbf42b8016
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-02-25 23:47:41 +0100

    feat: middlewares and userController functions 🚀

---
+++ README.md
@@ -0,0 +1,12 @@
+// - **POST /api/users** - Register new user
+// - **POST** `/api/users/auth` - Authenticate user and set token
+// - **POST** `/api/users/logout` - Logout user and clear cookie
+
+// - **GET** `/api/users` - Get all users
+// - **GET** `/api/users/profile` - Get user profile
+// - **GET** `/api/users/:id` - Get user by ID
+
+// - **PUT** `/api/users/profile` - Update user profile
+// - **PUT** `/api/users/:id` - Update user by ID
+
+// - **DELETE** `/api/users/:id` - Delete user
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('Server is ready now!!!');
@@ -13,16 +21,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// - **POST /api/users** - Register new user
-// - **POST** `/api/users/auth` - Authenticate user and get token
-// - **POST** `/api/users/logout` - Logout user and clear cookie
-
-// - **GET** `/api/users` - Get all users
-// - **GET** `/api/users/profile` - Get user profile
-// - **GET** `/api/users/:id` - Get user by ID
-
-// - **PUT** `/api/users/profile` - Update user profile
-// - **PUT** `/api/users/:id` - Update user by ID
-
-// - **DELETE** `/api/users/:id` - Delete user
