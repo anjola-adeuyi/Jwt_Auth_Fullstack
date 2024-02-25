cbb66f18435473c45ab8f37794f5b73c31ca2de7
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-02-25 22:42:55 +0100

    feat: structure api calls and routes ⚙️

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 import express from 'express';
-const port = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Server is ready');
+  res.send('Server is ready now!!!');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// - **POST /api/users** - Register new user
+// - **POST** `/api/users/auth` - Authenticate user and get token
+// - **POST** `/api/users/logout` - Logout user and clear cookie
+
+// - **GET** `/api/users` - Get all users
+// - **GET** `/api/users/profile` - Get user profile
+// - **GET** `/api/users/:id` - Get user by ID
+
+// - **PUT** `/api/users/profile` - Update user profile
+// - **PUT** `/api/users/:id` - Update user by ID
+
+// - **DELETE** `/api/users/:id` - Delete user
