677ceaec72c82dcb47eea4a95775ae0013035f85
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-02-26 01:46:06 +0100

    feat: Auth protect middleWare

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
